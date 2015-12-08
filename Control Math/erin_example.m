ca6f0ea085bdf083a518d8accda9346b6a9ce467
Erin Pierce <erinpierce13@gmail.com>
2015-12-07 22:26:39 -0500

    Erin learns how to tame the segway

---
+++ Control Math/erin_example.m
@@ -0,0 +1,32 @@
+%These constants are for the segway by itself
+rp0 = .03; %m
+mp0 = 10; %kg
+Ip0 = 0.274; %kg*m^2
+
+rp = .5; %m
+mp = 70; %kg
+Ip = 40; %kg*m^2
+
+
+k = 0.06; %V*s/rad
+R = 0.22; %Ohms
+L = 0; %Henries
+
+s = tf('s');
+[VtoWs, VtoU, VtoTheta] = generateTF(k,R,L,rp0,mp0,Ip0);
+%[VtoWs, VtoU, VtoTheta] = generateTF(k,R,L,rp,mp,Ip);
+
+Kv = 10;
+
+VctoWs = minreal(VtoWs/ (1+Kv*VtoWs));
+VctoU = minreal(VtoU/ (1+Kv*VtoWs));
+VctoTheta = minreal(VtoTheta/ (1+Kv*VtoWs));
+
+Compensator = (s+3)/s;
+%rlocus(Compensator*-VctoTheta);
+
+Kloop = 100;
+
+System = minreal(-Kloop*Compensator*VctoTheta/(1-Kloop*Compensator*VctoTheta));
+
+step(System);
