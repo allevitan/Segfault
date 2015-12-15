8f99019b201ac9cbf9fb5e95e4085cdf959bede5
ptitchener <paul.titchener@students.olin.edu>
2015-12-15 18:41:24 -0500

    Adding control matlab code for Paul to work on

---
+++ Control Math/my_example_Paul.m
@@ -0,0 +1,57 @@
+%These constants are for the segway by itself
+rp0 = .03; %m
+mp0 = 10; %kg
+Ip0 = 0.274; %kg*m^2
+
+rp = .5; %m
+mp = 70; %kg
+Ip = 40; %kg*m^2
+
+
+k = 0.06; %V*s/rad
+R = 0.22; %Ohms
+L = 0; %Henries
+
+s = tf('s');
+
+[VtoWs, VtoU, VtoTheta] = generateTF(k,R,L,rp0,mp0,Ip0);
+%[VtoWs, VtoU, VtoTheta] = generateTF(k,R,L,rp,mp,Ip);
+
+Kv = .6;
+
+VctoWs = minreal(Kv*VtoWs / (1+Kv*VtoWs));
+VctoU = minreal(Kv*VtoU / (1+Kv*VtoWs));
+VctoTheta = minreal(Kv*VtoTheta / (1+Kv*VtoWs));
+
+%rlocus(VtoWs);
+
+%pzmap(VctoTheta);
+
+Kv2 = 10;
+
+
+
+
+
+VcctoWs = minreal(Kv2*VctoWs/ (1+Kv2*VctoTheta*s));
+VcctoU = minreal(Kv2*VctoU/ (1+Kv2*VctoTheta*s));
+VcctoTheta = minreal(Kv2*VctoTheta/ (1+Kv2*VctoTheta*s));
+
+%rlocus(VctoTheta*s)
+
+pzmap(VcctoTheta);
+
+Compensator = (s+3)/(s);
+%rlocus(-Compensator*VctoTheta);
+
+Kloop = 200;
+
+systemPID = minreal(-Kloop*VctoTheta/(1-Kloop*VctoTheta));
+systemComp = minreal(-Kloop*Compensator*VctoTheta/(1-Kloop*Compensator*VctoTheta));
+
+%System = minreal(-Kloop*Compensator*VctoTheta/(1-Kloop*Compensator*VctoTheta));
+System = minreal(-Kloop*Compensator*VcctoTheta/(1-Kloop*Compensator*VcctoTheta));
+
+t = linspace(0,10,1000);
+u = [1,1, zeros(1,length(t)-2)];
+%lsim(System,u,t,1);
