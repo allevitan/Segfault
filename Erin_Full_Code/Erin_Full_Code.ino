3744ed088d229d2ca89084ec9d34797a2f8aef7a
Abraham Levitan <allevitan@gmail.com>
2015-12-15 23:42:33 -0500

    SAFETY!!!

--- Erin_Full_Code/Erin_Full_Code.ino
+++ Erin_Full_Code/Erin_Full_Code.ino
@@ -16,7 +16,7 @@ int16_t ax2, ay2, az2;
 int16_t gx2, gy2, gz2;
 float pi=3.141592;
 // theta is in degrees, omega is in degrees/second
-float theta, omega, theta_a, theta_g,thetam1;
+float theta, omega, theta_a, theta_g;
 
 // Encoder Variables
 int16_t raw_encoder_data[2] = {0,0};
@@ -27,10 +27,15 @@ float encoder_LS = 0;
 const float kv = 0.6; // in volts per radian
 const float kv_converted = kv * float(127)/24; // In steps per radian
 float omega_error;
-float Vc=0;
-float Vcm1=0;
+float Vc=0; float Vcm1 = 0; float Vcm2 = 0;
+float thetam1=0; float thetam2=0;
 Timer myTimer;
 float dt=20; 
+
+//Safety Parameters
+const float THETA_KILLYOFAMILY = 40;
+int SAFETY_PIN = 33;
+bool ALLGOODCAPN = true;
 
 uint8_t status = 1;
 const uint8_t myLED = BLUE_LED;
@@ -47,6 +52,9 @@ void setup()
 { 
   pinMode(myLED, OUTPUT);
   digitalWrite(myLED, HIGH);
+
+  pinMode(SAFETY_PIN, INPUT);
+
   Wire.begin();
   Serial.begin(9600);
   delay(50);
@@ -77,10 +85,37 @@ if (go==true){
   //Vc = 15.38*(theta*(pi/180))-(14.62*(thetam1*(pi/180)))+Vcm1;
   
   //Limiting and stuff
-  Vc=max(min(Vc,15),-15);
-  thetam1=theta;
-  Vcm1=Vc;
-  VelocityControlMotorRS(Vc); 
-  VelocityControlMotorLS(Vc);
+
+  Protect();
+  if (ALLGOODCAPN){
+    VelocityControlMotorRS(Vc); 
+    VelocityControlMotorLS(Vc);
+  } else {
+    driveMotorRS(0);
+    driveMotorLS(0); 
+  }
+  UpdateVars();
 } 
+}
+
+
+void Protect(){
+  digitalWrite(RED_LED,!digitalRead(SAFETY_PIN));
+  if ((abs(theta) > THETA_KILLYOFAMILY) || digitalRead(SAFETY_PIN)){
+    ALLGOODCAPN = false;
+    Vc = 0;
+    Vcm1 = 0;
+    Vcm2 = 0;
+  } else {
+    ALLGOODCAPN = true;
+  }
+  Vc=max(min(Vc,15),-15);
+}
+
+
+void UpdateVars(){
+  thetam2=thetam1;
+  thetam1=theta;
+  Vcm2=Vcm1;
+  Vcm1=Vc; 
 }
